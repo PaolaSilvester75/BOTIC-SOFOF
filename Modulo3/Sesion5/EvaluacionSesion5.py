cf478c39c9d2c1b38785ec04853ff823c706c544
PaolaSilvester75 <paola2094@e-camp.cl>
2024-11-07 20:44:15 -0300

    Delete Modulo3 directory

--- Modulo3/Sesion5/EvaluacionSesion5.py
+++
@@ -1,8 +0,0 @@
-palabra = "paralelepípedo"
-vocales = "aeiouáéíóú"
-
-# Recorremos la palabra 
-for i, letra in enumerate(palabra):
-    if letra.lower() not in vocales:
-        # Imprimimos la consonante y su posición
-        print(f"Consonante: {letra}, Posición: {i}")
